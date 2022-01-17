d5a529aef10899e587da3c75e1bf81b71964f434
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2022-01-17 11:11:31 -0800

    Example of nested model

---
+++ multilevel_megan/_READMEmultilevelmegan.txt
@@ -0,0 +1,17 @@
+Started 16 Jan 2022
+Code from meeting with Megan in 2017 ... 
+But with some extra info
+
+
+Some files here are VERY similar to a couple other files that Lizzie has:
+
+Basically identical (except for half a sentence of notes):
+	example-git/multilevel_megan/threelevel_plotsinsites.stan
+	stan/usefulcode/Oahu2017/threelevel_plotsinsites.stan
+
+Ditto (but with a few more differences):
+	stanexample_plotsinsites.R 
+	generatedata_threelevel_plotsinsites.R 
+
+Want another example of nested model? 
+	See nointer_3levelwpop_force&photo_ncp.stan in OSPREE (analyses/ranges/stan/)
---
+++ multilevel_megan/stanexample_plotsinsites.R
@@ -0,0 +1,79 @@
+#STAN example - plots in sites
+
+# Megan says (8 Jan 2017 gmail) ... The current version assumes that all sites have the same within-site (plot-to-plot) variance.  The data is generated that way, and the model is fit that way. ... In both the data generation code and the stan code, there are lines commented out that will generate/estimate separate within-site variances for each site.  When they are separate, these within-site variances are NOT pooled.  
+
+library(rstan)
+library(shinystan)
+
+nreps = 20  #replicate observations per plot
+nplots = 4  #plots per site
+S = 10     #total number of sites
+J = nplots * S  #total number of plots (4 per site)
+N = nreps*nplots*10 
+
+x <- rgamma(N,5,2)
+
+plotnum <- rep(c(1:J),each=nreps)   #list of plot numbers for each observation (length N)
+sitenum <- rep(c(1:10),each=nplots)  #list of site numbers for each plot (length J)
+
+#mu_a and sig_a are the mean and variance of the intercept across sites
+mu_a <- 5
+sig_a <- 2
+#mu_b and sig_b are the mean and variance of the slope across sites
+mu_b <- 4
+sig_b <- 1
+#draw S=10 site means for slope and intercept
+a_site <- rnorm(S,mu_a,sig_a)
+b_site <- rnorm(S,mu_b,sig_b)
+#draw S=10 site sds for slope and intercept (variability of plots within sites)
+# sig_a_site <- rlnorm(S,0,1)
+# sig_b_site <- rlnorm(S,0,1)
+#OR draw a single sd for all sites
+sig_a_site <- rlnorm(1,0,1)
+sig_b_site <- rlnorm(1,0,1)
+
+
+#for each plot, draw the slope and intercept from the appropriate site mean and sd
+a_plot <- rep(0,J)
+b_plot <- rep(0,J)
+# for (j in 1:J){
+#   a_plot[j] <- rnorm(1,a_site[sitenum[j]], sig_a_site[sitenum[j]]);
+#   b_plot[j] <- rnorm(1,b_site[sitenum[j]], sig_b_site[sitenum[j]]);
+# }
+#Alternatively, assume same within-site variance for all sites
+for (j in 1:J){
+  a_plot[j] <- rnorm(1,a_site[sitenum[j]], sig_a_site[1]);
+  b_plot[j] <- rnorm(1,b_site[sitenum[j]], sig_b_site[1]);
+}
+
+
+#draw three observations from each plot with sd = sig_y
+sig_y <- 1
+y <- rep(0,N)
+for (n in 1:N){
+  yhat[n] <- a_plot[plotnum[n]] + b_plot[plotnum[n]]*x[n]
+  y[n] <- a_plot[plotnum[n]] + b_plot[plotnum[n]]*x[n] + rnorm(1,0,sig_y)
+}
+
+#plot data
+allsites <- rep(c(1:10),each=12) #for plotting, list of site numbers for each obs
+plot(y~x,col=allsites,type='n')
+text(x,y,plotnum, cex=0.5, col=allsites)
+
+#call stan model
+dat <- list(N=N,S=S,J=J,plotnum=plotnum, sitenum=sitenum,y=y,x=x) 
+fitme <- stan("My Examples/threelevel_plotsinsites.stan", data=c("N","J","S","plotnum", "sitenum","y","x"), iter=4000, chains=4, control=list(adapt_delta=.9))
+
+mu_a
+mu_b
+sig_a
+sig_b
+sig_y
+a_plot
+b_plot
+a_site
+b_site
+sig_a_site
+sig_b_site
+
+launch_shinystan(fitme)
